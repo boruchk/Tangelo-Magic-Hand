de4e9a3a02f4ff7a87566b31fb531d299738047b
amh9766 <amh9766@nyu.edu>
2025-05-05 16:40:34 -0400

    Clean up code, adjust gyroscope control

--- tangelo_magic_hand/tangelo_magic_hand.ino
+++ tangelo_magic_hand/tangelo_magic_hand.ino
@@ -12,66 +12,79 @@ TODO:
 #include "bluetooth_setup.h"
 #include "sensor_setup.h"
 
+// Device pin constants 
+#define ACCEL_MOUSE 33
+#define GYRO_MOUSE 15
+#define BUTTON_PIN 32
 
+// Mouse button constants
 #define MOUSE_LEFT 1
 #define MOUSE_RIGHT 2
 #define MOUSE_MIDDLE 4
 #define MOUSE_BACK 8
 #define MOUSE_FORWARD 16
 
-#define ACCEL_MOUSE 33
-#define GYRO_MOUSE 15
-#define BUTTON_PIN 32
-
+// Gyroscope-related constants
+#define ROT_X_BOUND PI / 4
+#define ROT_Y_BOUND PI / 6
+#define ROT_X_CORRECT 0.035f
+#define ROT_Y_CORRECT 0.02f
 
 // LSM6DSOX chip number 1
 // Rename to "mouse", "Button", etc when finishing
 AS_LSM6DSOX sox1;
 AS_LSM6DSOX sox2;
 
+signed char mouse_move_x = 0,
+            mouse_move_y = 0,
+            mouse_v_wheel = 0,
+            mouse_h_wheel = 0;
+uint8_t     mouse_button_press = 0;
 
-signed char MOVE_x=0, MOVE_y=0, vWheel=0, hWheel=0;
-uint8_t buttonPress = 0;
-
-float x_degrees = 0.0f;
-float y_degrees = 0.0f;
-
-float previous_time = 0.0f;
-float cur_time = 0.0f;
+float x_radians = 0.0f;
+float y_radians = 0.0f;
 
 int dt = 10;  // polling frequency in ms 
 
+static inline void flush()
+{
+    if (can_communicate()) 
+    {
+        uint8_t mouse[5];
+        mouse[0] = mouse_button_press;  // Button
+        mouse[1] = mouse_move_x;        // Move in X
+        mouse[2] = mouse_move_y;        // Move in Y
+        mouse[3] = mouse_v_wheel;       // Vertical scroll
+        mouse[4] = mouse_h_wheel;       // Horizontal scroll
 
-static inline void move(signed char x, signed char y, signed char vWheel, signed char hWheel) {
-  // Check if the client is connected and it wants to receive messages from us.
-  // This also ensures that we only send data when the devices are paired and not just connected
-  // We check if the client is ready with getNotifications()
-  if (can_communicate()) {
-    uint8_t mouseMove[5];
-    mouseMove[0] = buttonPress;  // Button
-    mouseMove[1] = x;            // Move in X
-    mouseMove[2] = y;            // Move in Y
-    mouseMove[3] = vWheel;       // Vertical scroll
-    mouseMove[4] = hWheel;       // Horizontal scroll
+        inputMouse->setValue(mouse, sizeof(mouse));
+        inputMouse->notify();
 
-    inputMouse->setValue(mouseMove, sizeof(mouseMove));
-    inputMouse->notify();
+        if (mouse_move_x != 0 || mouse_move_y != 0)
+        {
+            Serial.println("Moving mouse cursor:"); 
+            Serial.println("X:"); 
+            Serial.println(mouse_move_x);
+            Serial.println("Y:"); 
+            Serial.println(mouse_move_y);
+        }
+        if (mouse_button_press)
+        {
+            Serial.println("Performing mouse button press:");
+            if (mouse_button_press == MOUSE_LEFT) Serial.println("Left click");
+        }
 
-    Serial.println("Moving mouse...");
-    Serial.print("X = ");
-    Serial.println(mouseMove[1]);
-    Serial.print("Y = ");
-    Serial.println(mouseMove[2]);
-  }
+        mouse_move_x = 0;
+        mouse_move_y = 0;
+        mouse_v_wheel = 0;
+        mouse_h_wheel = 0;
+        mouse_button_press = 0;
+    }
 }
 
-static inline void click(uint8_t button) {
-  buttonPress = button;
-  move(0,0,0,0);
-  buttonPress = 0;
-  move(0,0,0,0);
+static inline void use_button(uint8_t button) {
+    mouse_button_press = button;
 }
-
 
 static inline void readGyro(AS_LSM6DSOX& sox, const int chipSelect) {
   if (can_communicate()) {
@@ -83,56 +96,46 @@ static inline void readGyro(AS_LSM6DSOX& sox, const int chipSelect) {
 
     sox.getEvent(&accel, &gyro);
 
-    float time = (float) millis() / 1000;
-    float delta_time = time - previous_time;
-    previous_time = time;
-
-    
     Serial.print("\t\tGyro: \n");
-    Serial.print("X: ");
+    Serial.println("X:");
     Serial.print(gyro.gyro.x);
+    Serial.println(" rad/s ");
 
-    char temp_x = gyro.gyro.x;
-    x_degrees += gyro.gyro.x * delta_time;
-    MOVE_x = temp_x;
+    float rot_x = gyro.gyro.x * dt / 1000;
+    x_radians += rot_x;
+    mouse_move_x = rot_x;
 
-    float x_bound = PI / 4;
-    float y_bound = PI / 6;
-    float x_corrective_factor = 0.035;
-    float y_corrective_factor = 0.02;
-
-    if (x_degrees >= x_bound - x_corrective_factor) {
-        x_degrees = x_bound;
-        MOVE_x=10;
+    if (x_radians >= ROT_X_BOUND - ROT_X_CORRECT) {
+        mouse_move_x = 10;
     }
-    if (x_degrees <= -x_bound + x_corrective_factor) {
-        x_degrees = -x_bound;
-        MOVE_x=-10;
+    if (x_radians <= -ROT_X_BOUND + ROT_X_CORRECT) {
+        mouse_move_x = -10;
     }
 
+    Serial.println(x_radians);
 
-    Serial.print(" Y: ");
+    Serial.println("Y:");
     Serial.print(gyro.gyro.y);
-    char temp_y = gyro.gyro.y;
-    y_degrees += gyro.gyro.y * delta_time;
-    MOVE_y = temp_y;
+    Serial.println(" rad/s ");
 
-    if (y_degrees >= y_bound - y_corrective_factor) {
-        y_degrees = y_bound;
-        MOVE_y=10;
+    float rot_y = gyro.gyro.y * dt / 1000;
+    y_radians += rot_y;
+    mouse_move_y = rot_y;
+
+    if (y_radians >= ROT_Y_BOUND - ROT_Y_CORRECT) {
+        y_radians = ROT_Y_BOUND;
+        mouse_move_y = 10;
     }
-    if (y_degrees <= -y_bound + y_corrective_factor) {
-        y_degrees = -y_bound;
-        MOVE_y=-10;
+    if (y_radians <= -ROT_Y_BOUND + ROT_Y_CORRECT) {
+        y_radians = -ROT_Y_BOUND;
+        mouse_move_y = -10;
     }
 
-    Serial.print(" ");
-    Serial.print(y_degrees);
-
+    Serial.println(y_radians);
     
-    Serial.print(" Z: ");
+    Serial.println("Z: ");
     Serial.print(gyro.gyro.z);
-    Serial.println(" radians/s ");
+    Serial.println(" rad/s ");
     Serial.println();
 
     // Deselect the chip
@@ -165,8 +168,8 @@ void loop() {
   if (digitalRead(GYRO_MOUSE) == HIGH) {
     // Zero the gyroscope reading
     if (digitalRead(BUTTON_PIN) == HIGH) {
-        x_degrees = 0.0f;
-        y_degrees = 0.0f;
+        x_radians = 0.0f;
+        y_radians = 0.0f;
     }
 
     digitalWrite(LSM_CS2, HIGH);
@@ -178,13 +181,8 @@ void loop() {
     readAccel(sox1, LSM_CS1);
   }
 
-
-  move(MOVE_x, MOVE_y, vWheel, hWheel);
-  MOVE_x = 0;
-  MOVE_y = 0;
-
-
-  if (checkForDoubleTap(sox1)) click(MOUSE_LEFT);
-
-  delay(10);
+  if (checkForDoubleTap(sox1)) use_button(MOUSE_LEFT);
+  
+  flush();
+  delay(dt);
 }
